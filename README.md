7106c2782ec7565396401db490a7634b4fa038c0
Natalie Weaver <bmc.elementary.natalie@gmail.com>
2025-09-10 03:16:04 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mouseville Simulation
+
+Created for Berkeley Math Circle, Fall 2025, Level 2 (Grades 3-4).
---
+++ simulation.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+import streamlit as st
+
+st.header("Mouseville Simulation")
+
+st.markdown("""
+         How many shortest paths does each mouse have to get from school to
+         home? We can run an experiment to try to guess the answer.
+
+        All shortest paths are made of six left (`L`) or right (`R`) moves.
+         Flipping a coin six times (once at each intersection), we go left if we 
+         get heads, or right if we get tails. Each possible path is as likely as
+         any other: for example, we have any equal chance of getting `LLLLLL`,
+         `RRRRRR`, `LRRLRL`, or any other particular path.
+
+        Let's do this many times and keep track of where we end up. Since each
+         path is equally likely, the mouse's house we end up at the most often
+         probably has the most paths leading to it.
+""")
+
+min_trials = 25
+max_trials = 100000
+trials = st.number_input(
+    f"Enter a number between {min_trials:,} and {max_trials:,} to run the experiment that many times:",
+    min_value=min_trials,
+    max_value=max_trials,
+    value=25,
+    step=25,
+)
+
+routes = ["".join(np.random.choice(["R", "L"], size=6)) for _ in range(trials)]
+results = [route.count("R") for route in routes]
+
+df = pd.DataFrame({
+    "trial": range(trials),
+    "route": routes,
+    "result": results,
+})
+
+tab1, tab2 = st.tabs(["Plot", "Data"])
+
+with tab1:
+    fig = px.histogram(
+        df, x="result", title=f"Simulation with {trials:,} Trials", text_auto=True,
+    )
+    st.plotly_chart(fig)
+
+with tab2:
+    st.dataframe(df, hide_index=True)
